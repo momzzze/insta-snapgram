547cfb7389908b29266352a151909bd6ea78d3f2
nikola.ninov <momzzze@abv.bg>
2024-01-24 09:53:50 +0200

    add routing folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 import "./globals.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello snapgram!</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
